5109ba560d3633474a36d28b2de42034e76f455e
SirJohnK <SirJohnK@users.noreply.github.com>
2019-10-19 00:01:12 +0200

    Added base test classes for all extension methods.

---
+++ Tests/CleanWhitespaceTests.cs
@@ -0,0 +1,12 @@
+﻿using Awesome.StringExtensions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class CleanWhitespaceTests
+    {
+    }
+}
